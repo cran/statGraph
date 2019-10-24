0ba7fbf560a0903196fa107987ae17b62314d0c6
Andre Fujita <fujita@ime.usp.br>
2019-10-24 07:30:02 +0000

    version 0.3.0

---
+++ R/statGraph-package.R
@@ -0,0 +1,22 @@
+#' \packageDescription{statGraph}
+#'
+#' The DESCRIPTION file:
+#' \tabular{ll}{
+#' Package: \tab statGraph\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.3.0\cr
+#' Date: \tab 2019-10-23\cr
+#' Depends: \tab R (>= 2.10.0), stats, graphics\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab https://www.ime.usp.br/~fujita/software.html\cr
+#' }
+#'
+#' @title
+#' \packageTitle{statGraph}
+#' @author
+#' \packageAuthor{statGraph}
+#'
+#' Maintainer: \packageMaintainer{statGraph}
+"_PACKAGE"
